926aabbc339ef71b81bf220ff159eb3eb62f221e
tomasz <t.limisiewicz@gmail.com>
2020-05-03 03:48:36 +0200

    lab08: Tagger competition

--- labs/08/tagger_competition.py
+++ labs/08/tagger_competition.py
@@ -12,30 +12,178 @@ from morpho_dataset import MorphoDataset
 
 class Network:
     def __init__(self, pdt, args):
-        # TODO: Define a suitable model.
+
+        num_words = len(pdt.train.data[morpho.train.FORMS].words)
+        num_tags = len(pdt.train.data[morpho.train.TAGS].words)
+        num_chars = len(pdt.train.data[morpho.train.FORMS].alphabet)
+
+        word_ids = tf.keras.layers.Input(shape=[None])
+        wle = tf.keras.layers.Embedding(num_words, args.we_dim, mask_zero=True)(word_ids)
+        charseqs = tf.keras.layers.Input(shape=[None, None])
+        valid_words = tf.where(word_ids != 0)
+
+        cle = self.cle_embedding(charseqs, valid_words, num_chars, args)
+
+        hidden = tf.keras.layers.Concatenate()([wle, cle])
+
+        args.rnn_cell_dim = args.we_dim + args.cle_dim * 2
+
+        hidden = self.bidirectional_rnn(hidden, args)
+
+        predictions = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(num_tags, activation='softmax'))(hidden)
+
+        self.model = tf.keras.Model(inputs=[word_ids, charseqs], outputs=predictions)
+        self.model.compile(optimizer=self.get_optimizer(args),
+                           loss=tf.losses.SparseCategoricalCrossentropy(),
+                           metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")])
 
         self._writer = tf.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
 
+    def cle_embedding(self, charseqs, valid_words, num_chars, args):
+        cle = tf.gather_nd(charseqs, valid_words)
+        cle = tf.keras.layers.Embedding(num_chars, args.cle_dim, mask_zero=True)(cle)
+
+        if args.rnn_cle:
+            print('aaaa!')
+        cle = tf.keras.layers.Bidirectional(
+                tf.keras.layers.GRU(args.cle_dim, return_sequences=False, name="CL_GRU"), merge_mode='concat')(cle)
+
+        cle = tf.scatter_nd(valid_words, cle, [tf.shape(charseqs)[0], tf.shape(charseqs)[1], cle.shape[-1]])
+        return cle
+
+    def bidirectional_rnn(self, hidden, args):
+        residual = hidden
+        for _ in range(args.num_layers):
+            if args.rnn_cell == 'LSTM':
+                hidden = tf.keras.layers.Bidirectional(
+                    tf.keras.layers.LSTM(args.rnn_cell_dim, return_sequences=True, name="LSTM"), merge_mode='sum')(hidden)
+            elif args.rnn_cell == 'GRU':
+                hidden = tf.keras.layers.Bidirectional(
+                    tf.keras.layers.GRU(args.rnn_cell_dim, return_sequences=True, name="GRU"), merge_mode='sum')(hidden)
+            else:
+                hidden = tf.keras.layers.Bidirectional(
+                    tf.keras.layers.SimpleRNN(args.rnn_cell_dim, return_sequences=True, name="GRU"), merge_mode='sum')(hidden)
+
+            hidden += residual
+
+        return hidden
+
     def train(self, pdt, args):
-        # TODO: Train the network on a given dataset.
-        raise NotImplementedError()
+        for _ in range(args.epochs):
+            for train_batch in pdt.train.batches(args.batch_size):
+                self.train_batch(train_batch)
+
+            self.evaluate(pdt.dev, 'validation', args)
+
+    def train_batch(self, batch):
+        metrics = self.model.train_on_batch([batch[MorphoDataset.Dataset.FORMS].word_ids,
+                                             batch[MorphoDataset.Dataset.FORMS].charseqs],
+                                            batch[MorphoDataset.Dataset.TAGS].word_ids,
+                                            reset_metrics=True)
+
+
+        # Generate the summaries each 100 steps
+        if self.model.optimizer.iterations % 100 == 0:
+            tf.summary.experimental.set_step(self.model.optimizer.iterations)
+            with self._writer.as_default():
+                for name, value in zip(self.model.metrics_names, metrics):
+                    tf.summary.scalar("train/{}".format(name), value)
+
+
+    def evaluate(self, dataset, dataset_name, args):
+        self.model.reset_metrics()
+        for batch in dataset.batches(args.batch_size):
+            metrics = self.model.test_on_batch([batch[dataset.FORMS].word_ids, batch[dataset.FORMS].charseqs],
+                                               batch[dataset.TAGS].word_ids,
+                                               reset_metrics=False)
+
+        metrics = dict(zip(self.model.metrics_names, metrics))
+        with self._writer.as_default():
+            tf.summary.experimental.set_step(self.model.optimizer.iterations)
+            for name, value in metrics.items():
+                tf.summary.scalar("{}/{}".format(dataset_name, name), value)
+                print()
 
     def predict(self, dataset, args):
-        # TODO: The `predict` method should return a list, each element corresponding
-        # to one sentence. Each sentence should be a list/np.ndarray
-        # containing _indices_ of chosen tags (not the logits/probabilities).
-        raise NotImplementedError()
+
+        predictions = []
+        for batch in dataset.batches(args.batch_size):
+            prediction = self.model([batch[dataset.FORMS].word_ids, batch[dataset.FORMS].charseqs])
+            predictions += prediction
+
+        return predictions
+
+    def test(self, pdt, args):
+        out_path = "tagger_competition_test.txt"
+        if os.path.isdir(args.logdir): out_path = os.path.join(args.logdir, out_path)
+        with open(out_path, "w", encoding="utf-8") as out_file:
+            for i, sentence in enumerate(self.predict(morpho.test, args)):
+                for j in range(len(morpho.test.data[morpho.test.FORMS].word_strings[i])):
+                    print(morpho.test.data[morpho.test.FORMS].word_strings[i][j],
+                          morpho.test.data[morpho.test.LEMMAS].word_strings[i][j],
+                          morpho.test.data[morpho.test.TAGS].words[sentence[j]],
+                          sep="\t", file=out_file)
+                print(file=out_file)
+
+
+    def get_optimizer(self, args):
+        learning_rate_final = args.learning_rate_final
+        decay_steps = int(90828 * args.epochs / args.batch_size)
+        if args.decay == 'polynomial':
+            learning_rate_schedule = tf.optimizers.schedules.PolynomialDecay(args.learning_rate,
+                                                                             decay_steps=decay_steps,
+                                                                             end_learning_rate=args.learning_rate_final)
+        elif args.decay == 'exponential':
+            decay_rate = learning_rate_final / args.learning_rate
+            learning_rate_schedule = tf.optimizers.schedules.ExponentialDecay(args.learning_rate,
+                                                                              decay_steps=decay_steps,
+                                                                              decay_rate=decay_rate, staircase=False)
+        elif args.decay == "onplateau":
+            learning_rate_schedule = args.learning_rate
+            self._callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5,
+                                                                        min_lr=args.learning_rate_final))
+
+        else:
+            learning_rate_schedule = args.learning_rate
+
+        optimizer = None
+        if args.optimizer == 'SGD':
+            optimizer = tf.optimizers.SGD(learning_rate=learning_rate_schedule, momentum=args.momentum)
+        elif args.optimizer == "RMSProp":
+            optimizer = tf.optimizers.RMSprop(learning_rate=learning_rate_schedule, momentum=args.momentum)
+        elif args.optimizer == 'Adam':
+            optimizer = tf.optimizers.Adam(learning_rate=learning_rate_schedule)
+
+        return optimizer
+
+    def save(self, curr_date, args):
+        self.model.save(os.path.join(args.logdir, "{}-{:.4f}-model.h5".format(curr_date, 0.00), include_optimizer=False))
 
 
 if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    # TODO: Define reasonable defaults and optionally more parameters
-    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+    # Training parameters
+    parser.add_argument("--batch-size", default=None, type=int, help="Batch size.")
     parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
     parser.add_argument("--seed", default=42, type=int, help="Random seed.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    # RNN architecture
+    parser.add_argument("--num-layers", default=2, type=int, help="Number of RNN layers")
+    parser.add_argument("--rnn-cell-dim", default=512, type=int, help="Dimensionality of RNN latent vector")
+    parser.add_argument("--rnn-cell", default='LSTM', type=str, help='Type of RNN cell (LSTM, GRU, or SimpleRNN')
+    # Embeddings
+    parser.add_argument("--we-dim", default=128, type=int, help="Dimensionality of word embeddings")
+    parser.add_argument("--cle-dim", default=64, type=int, help="Dimensionality of character level embeddings")
+    parser.add_argument("--rnn-cle", action='store_true', help="Whether to use RNN chracter level embeddings")
+    # Optimizer parameters
+    parser.add_argument("--optimizer", default='Adam', type=str, help="NN optimizer")
+    parser.add_argument("--momentum", default=0., type=float, help="Momentum of gradient schedule.")
+    parser.add_argument("--decay", default=None, type=str, help="Learning decay rate type")
+    parser.add_argument("--learning-rate", default=0.01, type=float, help="Initial learning rate.")
+    parser.add_argument("--learning-rate-final", default=None, type=float, help="Final learning rate.")
+
     parser.add_argument("--verbose", default=False, action="store_true", help="Verbose TF logging.")
     args = parser.parse_args([] if "__file__" not in globals() else None)
 
@@ -50,9 +198,10 @@ if __name__ == "__main__":
         os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
     # Create logdir name
+    curr_date = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(globals().get("__file__", "notebook")),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        curr_date,
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
@@ -64,15 +213,7 @@ if __name__ == "__main__":
     network = Network(morpho, args)
     network.train(morpho, args)
 
+
     # Generate test set annotations, but to allow parallel execution, create it
     # in in args.logdir if it exists.
-    out_path = "tagger_competition_test.txt"
-    if os.path.isdir(args.logdir): out_path = os.path.join(args.logdir, out_path)
-    with open(out_path, "w", encoding="utf-8") as out_file:
-        for i, sentence in enumerate(network.predict(morpho.test, args)):
-            for j in range(len(morpho.test.data[morpho.test.FORMS].word_strings[i])):
-                print(morpho.test.data[morpho.test.FORMS].word_strings[i][j],
-                      morpho.test.data[morpho.test.LEMMAS].word_strings[i][j],
-                      morpho.test.data[morpho.test.TAGS].words[sentence[j]],
-                      sep="\t", file=out_file)
-            print(file=out_file)
+
